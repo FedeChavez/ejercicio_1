66580179a87540ee02823541fbd807ba2ed46642
Federico Martin Chavez <FedeChavez@MacBook-Pro-de-Federico.local>
2017-08-28 13:10:35 -0300

    Initial Commit

---
+++ 1Ejercicio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  1Ejercicio
+//
+//  Created by Federico Martin Chavez on 28/8/17.
+//  Copyright © 2017 NextU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
